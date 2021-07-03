3ec4e682de70ca0319356c1241d50f4fdc4b407c
jackalstar88 <jackalstar88@gmail.com>
2021-07-03 12:23:12 +0530

    Add API Key handler file and add private keys to gitignore

---
+++ lib/keys/ApiKeys.dart
@@ -0,0 +1,6 @@
+// TO-DO: Remove the below import, this is used for my private key import.
+// TO-DO: Replace the variables with your own API Key.
+import 'package:stockup/keys/ApiKeys_Pvt.dart' as ApiKey;
+
+// Define your CoinMarketCap API Key here.
+const String cryptoApi = ApiKey.cryptoApi;
